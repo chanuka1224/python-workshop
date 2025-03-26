ccc1666aaafec862d18699ef91fa137c23c5c7ef
unknown <239154@TECH.WYB.AC.LK>
2025-03-26 21:26:05 +0530

    first commit

--- task.py
+++ task.py
@@ -1,5 +1,14 @@
-num=0
-while(num<100):
-    if(num%7==0):
-        print(num)
-    num+=1
+# num=[1,22,45,6,3,7,22,46,6,4,3,44]
+# num.sort(reverse=True)
+# print(num)
+# print(num.index(1))
+# print(len(num))
+
+def filter_value(num):
+    if(num>25):
+        return True
+    else:
+        return False
+price=[1,22,45,6,3,7,22,46,6,4,3,44]
+filter_value1=filter(filter_value,price)
+print(list(filter_value1))
